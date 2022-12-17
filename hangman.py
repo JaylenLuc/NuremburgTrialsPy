f7e1c55c97a31b75d4be2161049fe606a4e8bce1
JaylenLuc <JHLUC@UCI.EDU>
2022-12-17 14:29:35 -0800

    final working version

--- hangman.py
+++ hangman.py
@@ -281,15 +281,23 @@ class Game_runner:
 
 running = True
 cprint(figlet_format('  THE\nOFFICIAL\nHANG MAN\nCOMMAND\nLINE GAME', font='isometric3',width=120),
-        'blue', attrs=['bold'])
+        'blue', attrs=['bold', 'blink'])
 time.sleep(1)
 print(chr(27) + "[2J")
 cprint(figlet_format('  THE\nOFFICIAL\nHANG MAN\nCOMMAND\nLINE GAME', font='isometric1',width=120),
-'magenta', attrs=['bold'])
+'magenta', attrs=['bold', 'blink'])
 time.sleep(1)
 print(chr(27) + "[2J")
 cprint(figlet_format('  THE\nOFFICIAL\nHANG MAN\nCOMMAND\nLINE GAME', font='isometric3',width=120),
-'green', attrs=['bold'])
+'yellow', attrs=['bold', 'blink'])
+time.sleep(1)
+print(chr(27) + "[2J")
+cprint(figlet_format('  THE\nOFFICIAL\nHANG MAN\nCOMMAND\nLINE GAME', font='isometric1',width=120),
+'cyan', attrs=['bold', 'blink'])
+time.sleep(1)
+print(chr(27) + "[2J")
+cprint(figlet_format('  THE\nOFFICIAL\nHANG MAN\nCOMMAND\nLINE GAME', font='isometric3',width=120),
+'green', attrs=['blink','bold'])
 print()
 print()
 uu = ""
